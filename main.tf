3adecb9c088daeac4564e8ff3a0182a1b1fd8126
pgreene <pgreene@protonmail.com>
2024-08-22 16:59:19 -0400

    Updated module

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
-resource "aws_organizations_policy" "general" {
-  name = var.name
-  description = var.description
-  type = var.type
-  content = var.content
-  skip_destroy = var.skip_destroy
+resource "aws_organizations_account" "general" {
+  name  = var.name
+  email = var.email
+  close_on_deletion = var.close_on_deletion
+  create_govcloud = var.create_govcloud
+  iam_user_access_to_billing = var.iam_user_access_to_billing
+  parent_id = var.parent_id
+  role_name = var.role_name
   tags = var.tags
 }
